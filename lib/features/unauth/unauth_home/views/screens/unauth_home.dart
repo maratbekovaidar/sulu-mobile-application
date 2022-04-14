92ba1b55b7372e56b15bec2d3d5f10a53c7b68f6
Aidar Maratbekov <maratbekovaidar@gmail.com>
2022-04-15 05:16:21 +0600

    [#22] feat(unauth_home_page): change drawer with login and register button

--- lib/features/unauth/unauth_home/views/screens/unauth_home.dart
+++ lib/features/unauth/unauth_home/views/screens/unauth_home.dart
@@ -15,6 +15,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sulu_mobile_application/utils/repository/appointment_repository.dart';
 import 'package:sulu_mobile_application/utils/repository/banner_repository.dart';
 import 'package:sulu_mobile_application/utils/repository/establishment_repository.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 
 class UnAuthHomePage extends StatefulWidget {
@@ -37,6 +39,61 @@ class _UnAuthHomePageState extends State<UnAuthHomePage> {
     });
   }
 
+  Function? becomePartner() {
+    showDialog(context: context, builder: (_) {
+
+      TextEditingController _personController = TextEditingController();
+      TextEditingController _feedbackController = TextEditingController();
+
+
+
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        title: const Text("Напишите отчет", textAlign: TextAlign.center),
+        content: SizedBox(
+          height: 170,
+          child: Center(
+            child: Column(
+              children: [
+                TextField(
+                  controller: _personController,
+                  decoration: const InputDecoration(
+                      hintText: "Ваши контакты"
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _feedbackController,
+                  maxLines: 2,
+                  decoration: const InputDecoration(
+                    hintText: "Ваш предложение",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          TextButton(
+              onPressed: () async {
+                Navigator.pop(context);
+                String feedbackText = _feedbackController.value.text;
+                String personText = _personController.value.text;
+                String textOutput = "Контакты: " + personText + ".\nСообщение: " + feedbackText;
+                var url = Uri.parse('https://api.telegram.org/bot5058514406:AAHBko2SdNPqQIemzUUEcB0sRBUBwABJx98/sendMessage?chat_id=-1001658501273&text=$textOutput');
+                await http.post(url);
+              },
+              child: const Text("Отправить")
+          )
+        ],
+      );
+    });
+    return null;
+  }
+
+
   /// Repository
   EstablishmentRepository establishmentRepository = EstablishmentRepository();
   AppointmentRepository appointmentRepository = AppointmentRepository();
@@ -44,6 +101,7 @@ class _UnAuthHomePageState extends State<UnAuthHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
 
     /// Bottom Navbar pages
     List<Widget> pages = [
@@ -85,7 +143,43 @@ class _UnAuthHomePageState extends State<UnAuthHomePage> {
       child: Scaffold(
 
         /// Drawer
-        drawer: const SuluDrawer(),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+
+              /// Header
+              Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+
+
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        'assets/background/drawer.png',
+                      ),
+                    )
+                ),
+                height: 150,
+                child: Center(child: Image.asset('assets/logo/drawer_logo.png')),
+              ),
+              const SizedBox(height: 10),
+              ListTileItem(
+                onTap: (){
+                  Navigator.pushNamed(context, "/login");
+                },
+                text: "Войти", icon: Icons.login_rounded
+              ),
+              ListTileItem(
+                  onTap: (){
+                    Navigator.pushNamed(context, "/register");
+                  },
+                  text: "Зарегистрироваться", icon: Icons.app_registration_rounded
+              ),
+              ListTileItem( onTap:()async{_launchURL();}, text: "Стать партнером", icon: Icons.wallet_travel_rounded),
+              ListTileItem( onTap: (){becomePartner();}, text: "Техническая поддержка", icon: Icons.help_outline_rounded)
+            ],
+          ),
+        ),
 
         /// AppBar
         appBar: AppBar(
@@ -128,3 +222,11 @@ class _UnAuthHomePageState extends State<UnAuthHomePage> {
     );
   }
 }
+
+
+/// Open url of partners
+const String _url = 'https://sulu.life';
+Future _launchURL() async {
+  if (!await launch(_url)) throw 'Could not launch $_url';
+}
+
