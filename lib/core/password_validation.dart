dbfe77278ff1f67012c8d8e7d456e442d39cf32f
Aidar Maratbekov <maratbekovaidar@gmail.com>
2022-04-19 23:33:25 +0600

    [#24] feat(password_validator): initialize client

---
+++ lib/core/password_validation.dart
@@ -0,0 +1,14 @@
+/// **Validator must have: **
+///
+/// * IoC
+/// * Must expandable
+/// * Must have customizer
+/// * Have upper case text
+/// * Have number
+/// * Have lower case
+/// * Have digits
+/// * Have max and min password size
+/// * Have constructor of template exception
+class PasswordValidation {
+
+}
