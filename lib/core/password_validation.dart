65ce4b175a397aee015efdb7092589e7915beda2
Aidar Maratbekov <maratbekovaidar@gmail.com>
2022-04-20 00:07:10 +0600

    [#24] feat(password_validator): add main exceptions

--- lib/core/password_validation.dart
+++ lib/core/password_validation.dart
@@ -11,4 +11,9 @@
 /// * Have constructor of template exception
 class PasswordValidation {
 
+  final RegExp _upperCaseSymbols = RegExp(r'[A-Z]');
+  final RegExp _lowerCaseSymbols = RegExp(r'[a-z]');
+  final RegExp _digits = RegExp(r'[0-9]');
+  final RegExp _symbols = RegExp(r'[!@#<>?":_`~;[\]\\|=+)(*&^%\s-]');
+
 }
